444e0ab9c90e46336fd9f99953d65f7c389dea8a
Loic Chambost <chambost.loic@gmail.com>
2020-10-30 16:36:17 +0100

    upade

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 
 export default class Contact extends Component {
+
+  submitForm = this.submitForm.bind(this);
+  state = {
+    status: ""
+  };
+
   render() {
     return (
       <div className="contact section" id="Contact">
@@ -9,10 +15,12 @@ export default class Contact extends Component {
             <h2 className="text-center">Contact</h2>
           </div>
           <form
-            action={`https://formspree.io/${this.props.data}`}
+            action="https://formspree.io/f/xeqpkapr"
             name="contact"
             method="POST"
             data-netlify="true"
+            data-netlify-honeypot="bot"
+            onSubmit={this.submitForm}
           >
             <div>
               <label>
@@ -30,11 +38,35 @@ export default class Contact extends Component {
               </label>
             </div>
             <div>
-              <button type="submit">Envoyer</button>
+              
+              {this.status === "SUCCESS" ? <p>Merci!</p> : <button type="submit">Envoyer</button>}
+              {this.status === "ERROR" && <p>Ooops! Une erreur est survenue.</p>}
+
             </div>
           </form>
         </div>
       </div>
     );
   }
+
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
+
 }
+
